3049915a2972eae8e06a193a6a2200386789169a
David-Vandensteen <dvandensteen@gmail.com>
2023-10-29 18:52:50 +0100

    First

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import { MidiNormalizer } from 'midi-normalizer';
+
+let instance;
+
+export default class MidiControllerStore {
+  #cache = new Map();
+
+  static getInstance() {
+    if (!instance) instance = new MidiControllerStore();
+    return instance;
+  }
+
+  static decode(id) {
+    const controller = Math.floor(id / 128);
+    const channel = id % 128;
+    return { controller, channel };
+  }
+
+  static encode(controller, channel) { return controller * 128 + channel; }
+
+  getValue(controller, channel) {
+    if (controller === undefined) throw new Error('controller is undefined');
+    if (channel === undefined) throw new Error('channel is undefined');
+    const key = MidiControllerStore.encode(controller, channel);
+    return this.#cache.get(key) ?? 0;
+  }
+
+  clear() {
+    this.#cache.clear();
+    return this;
+  }
+
+  set(controller, channel, value) {
+    if (controller === undefined) throw new Error('controller is undefined');
+    if (channel === undefined) throw new Error('channel is undefined');
+    if (value === undefined) throw new Error('value is undefined');
+    const normalizedController = MidiNormalizer.controller(controller);
+    const normalizedChannel = MidiNormalizer.channel(channel);
+    const normalizedValue = MidiNormalizer.value(value);
+    this.#cache.set(
+      MidiControllerStore.encode(
+        normalizedController,
+        normalizedChannel,
+      ),
+      normalizedValue,
+    );
+    return this;
+  }
+}
+
+export {
+  MidiControllerStore,
+};
---
+++ test/midi-controller-store.test.js
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { MidiControllerStore } from '#src/index';
+
+describe('midi controller store', () => {
+  it('should return the same instance when calling getInstance multiple times', () => {
+    const instance1 = MidiControllerStore.getInstance();
+    const instance2 = MidiControllerStore.getInstance();
+    expect(instance1).to.equal(instance2);
+  });
+
+  it('should set and get the correct value for a given controller and channel', () => {
+    const state = MidiControllerStore.getInstance();
+    const controller = 1;
+    const channel = 2;
+    const value = 3;
+    state.set(controller, channel, value);
+    expect(state.getValue(controller, channel)).to.equal(3);
+  });
+
+  it('should return 0 for default values', () => {
+    const state = MidiControllerStore.getInstance();
+    state.clear();
+    expect(state.getValue(1, 1)).to.equal(0);
+    expect(state.getValue(2, 1)).to.equal(0);
+    expect(state.getValue(1, 2)).to.equal(0);
+    expect(state.getValue(2, 2)).to.equal(0);
+  });
+});
